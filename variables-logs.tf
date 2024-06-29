6d6c96c5f7f9365b1b87891f610ee02191573fa6
Cristian Beraha <cristian@cloudops.works>
2024-06-28 21:02:39 -0300

    chore: naming convention

--- variables-logs.tf
+++ variables-logs.tf
@@ -7,7 +7,7 @@
 variable "access_logs" {
   type = map(object({
     enabled              = bool
-    bucket               = string
+    bucket_name          = string
     logs_prefix          = optional(string, "")
     logs_retention_years = optional(number, 3)
     logs_archive_days    = optional(number, 30)
@@ -15,7 +15,7 @@ variable "access_logs" {
   default = {
     access_logs = {
       enabled              = false
-      bucket               = ""
+      bucket_name          = ""
       logs_prefix          = ""
       logs_retention_years = 3
       logs_archive_days    = 30
