06a1ed68061cc7421d6e7d5805d7519b1772d73d
Cristian Beraha <cristian@cloudops.works>
2025-01-31 15:37:47 -0300

    Add support for extra HTTPS listeners to ALB
    Introduce a new `extra_listeners` variable to define additional HTTPS listeners dynamically. Each listener includes a default fixed-response action for unauthorized access (401). This extends flexibility in configuring ALB listener behavior.

--- main.tf
+++ main.tf
@@ -99,6 +99,37 @@ resource "aws_lb_listener" "this_https" {
   )
 }
 
+# Extra listeners
+resource "aws_lb_listener" "extra_https" {
+  for_each = {
+    for listener in var.extra_listeners : "port-${listener.port}" => listener
+  }
+  depends_on = [
+    aws_acm_certificate_validation.default_cert
+  ]
+  load_balancer_arn = aws_lb.this.arn
+  port              = each.value.port
+  protocol          = "HTTPS"
+  ssl_policy        = var.ssl_policy
+  certificate_arn   = var.acm_certificate_arn != "" ? var.acm_certificate_arn : aws_acm_certificate.default_cert[0].arn
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "application/json"
+      message_body = "{\"error\": \"Not Allowed\"}"
+      status_code  = "401"
+    }
+  }
+  tags = merge(
+    local.all_tags,
+    {
+      Name = format("alb-%s-https", local.system_name_short)
+    }
+  )
+}
+
 # resource "aws_ec2_tag" "default_action_80" {
 #   for_each    = local.all_tags
 #   resource_id = aws_lb_listener.this_http.default_action[0].id
