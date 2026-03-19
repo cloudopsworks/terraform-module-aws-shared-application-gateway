5a7a78e1a17e687043cd4fc02400ba32452a957a
Cristian Beraha <cristian@cloudops.works>
2026-03-19 10:03:32 -0300

    chore: Upgrade repository from template, version: v1.5.4

--- variables-acm.tf
+++ variables-acm.tf
@@ -8,6 +8,7 @@
 #
 
 variable "default_ssl" {
+  description = "Configuration for the default SSL certificate to be used for the ALB listener. If enabled, the module will create a new ACM certificate with the specified Common Name (CN) and Subject Alternative Names (SANs). The certificate will be automatically validated using the specified validation method (DNS or Email) and validation domain/email. If auto_validation is set to true, the module will attempt to automatically validate the certificate using the provided information. If auto_validation is false, you will need to manually validate the certificate in ACM before it can be used by the ALB listener."
   type = object({
     enabled           = bool
     cn                = string
@@ -16,6 +17,7 @@ variable "default_ssl" {
     validation_method = optional(string, "DNS")
     validation_domain = optional(string, "")
     validation_email  = optional(string, "")
+    dns_ttl           = optional(number, 300)
   })
   default = {
     enabled           = false
@@ -30,7 +32,8 @@ variable "default_ssl" {
 }
 
 variable "acm_certificate_arn" {
-  type    = string
-  default = ""
+  description = "ARN of an existing ACM Certificate to use instead of creating a new one. If provided, the module will not create a new certificate and will use this ARN for the ALB listener. The certificate must be in the same region as the ALB and must be valid for the domain names used in the ALB listener rules."
+  type     = string
+  default  = ""
   nullable = false
 }
--- variables-alb.tf
+++ variables-alb.tf
@@ -9,58 +9,68 @@
 
 # Defaults to external ALB
 variable "is_internal" {
+  description = "Whether the ALB should be internal or internet-facing. If not specified, it will default to false (internet-facing)."
   type     = bool
   default  = false
   nullable = false
 }
 
 variable "ip_address_type" {
+  description = "The type of IP addresses used by the ALB. Valid values are ipv4 and dualstack. If not specified, it will default to ipv4."
   type     = string
   default  = "ipv4"
   nullable = false
 }
 
 variable "delete_protection" {
+  description = "Enable or disable deletion protection for the ALB. If not specified, it will default to true (enabled)."
   type     = bool
   default  = true
   nullable = false
 }
 
 variable "vpc_id" {
+  description = "The ID of the VPC to associate with the ALB. This variable is required and does not have a default value."
   type = string
 }
 
 variable "private_subnet_ids" {
+  description = "List of private subnet IDs to associate with the ALB. If not specified, it will default to an empty list (no subnets)."
   type     = list(string)
   default  = []
   nullable = false
 }
 
 variable "public_subnet_ids" {
+  description = "List of public subnet IDs to associate with the ALB. If not specified, it will default to an empty list (no subnets)."
   type     = list(string)
   default  = []
   nullable = false
 }
 
 variable "ssl_policy" {
+  description = "The security policy to apply to the HTTPS listener. If not specified, it will default to ELBSecurityPolicy-TLS-1-2-2017-01."
   type     = string
   default  = "ELBSecurityPolicy-TLS-1-2-2017-01"
   nullable = false
 }
 
 variable "cross_zone_load_balancing" {
+  description = "Enable or disable cross-zone load balancing. If not specified, it will default to true (enabled)."
   type     = bool
   default  = true
   nullable = false
 }
 
 variable "extra_listeners" {
+  description = "Additional listeners to create on the ALB. Each listener should be defined as an object with the following properties: protocol (string), port (number), ssl_policy (string, optional), certificate_arn (string, optional), default_action (object). If not specified, it will default to an empty list (no additional listeners)."
   type     = any
   default  = []
   nullable = false
 }
 
 variable "server_header_enabled" {
+  description = "Enable or disable the server header in HTTP responses. If not specified, it will default to false (disabled)."
   type     = bool
   default  = false
   nullable = false
